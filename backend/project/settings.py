a64347ffe266a2d36a863cb28055979921b4bed1
Ayman Bouabra <abouabra@c1r2p6.1337.ma>
2024-08-31 08:49:46 +0100

    ~ updated requirements.txt to work in MacOS, + set the project to use my custom User model

--- backend/project/settings.py
+++ backend/project/settings.py
@@ -16,6 +16,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+AUTH_USER_MODEL = "user_management.User"
 
 # Application definition
 
--- backend/user_management/models.py
+++ backend/user_management/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+
+class User(AbstractUser):
+    class Meta:
+        verbose_name_plural = "Users"
+
+    email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=True)
+
+    def __str__(self):
+        return f"{self.username}"

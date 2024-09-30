3f484628bf9ec769a420b4380fa6d3a752eee262
Ayman Bouabra <aymanbouabra@gmail.com>
2024-09-30 20:07:15 +0100

    modified all backend services to have a script enrtypoint for optimization

---
+++ backend/chat_service/entrypoint.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# Check if there are unapplied migrations and apply them if needed
+if [ "$(python manage.py showmigrations --plan | grep '\[ \]')" ]; then
+    echo "Applying migrations..."
+    python manage.py migrate --noinput
+else
+    echo "No migrations to apply."
+fi
+
+# Start the Django development server
+exec python manage.py runserver 0.0.0.0:8001

6e37c8f7d16f6a3485b2672fae083fd0a365714d
Mohamed Baanni <hamide185@gmail.com>
2024-11-07 20:52:28 +0100

    start on display tournament for four

--- backend/assets/games/space_invaders/js/three-defs.js
+++
@@ -1,5 +0,0 @@
-import * as THREE from '/assets/games/space_invaders/core/three.module.js';
-import {GLTFLoader} from '/assets/games/space_invaders/core/GLTFLoader.js';
-import Stats from '/assets/games/space_invaders/core/stats.module.js';
-
-export {THREE,  GLTFLoader, Stats};
